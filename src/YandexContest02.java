cd86173867bd5bef304a16acc680a61079a9c8aa
ojustine <ojustine@student.21-school.ru>
2020-03-07 00:02:51 +0300

    some works

--- src/YandexContest02.java
+++ src/YandexContest02.java
@@ -30,13 +30,11 @@ public class YandexContest02
 
 			for (int i = 0; i < requestsNum; i++)
 			{
-				request = buffReader.readLine().split("[\\s:]");
+				request = buffReader.readLine().split(" |:");
 				if ("APPIONT".equals(request[0]))
-				{
 					setMeet(request);
-				}
 				else if ("PRINT".equals(request[0]))
-					return;
+					printPersonMeets(request);
 				else
 					throw (new IllegalArgumentException("Bad request"));
 			}
@@ -54,9 +52,70 @@ public class YandexContest02
 		int duration = Integer.parseInt(request[4]);
 		int partySize = Integer.parseInt(request[5]);
 		String[] party = new String[partySize];
-		for (int j = 0; j < partySize; j++)
-			party[j] = request[j + 6];
-		meets.add(new Meet(time, duration, party));
+		if (canSetMeet(time, duration))
+		{
+			for (int j = 0; j < partySize; j++)
+				party[j] = request[j + 6];
+			meets.add(new Meet(time, duration, party));
+			System.out.println("OK");
+		}
+		else
+		{
+			System.out.println("FAIL");
+			for (String person : party)
+			{
+				System.out.print(person);
+				System.out.println();
+			}
+		}
+	}
+
+	public static boolean canSetMeet(int time, int duration)
+	{
+		for (Meet meet : meets)
+		{
+			if ((time >= meet.start && time <= meet.start + meet.duration)
+			|| (time + duration >= meet.start && time + duration <= meet.start + meet.duration))
+				return (false);
+		}
+		return (true);
+	}
+
+	public static void printPersonMeets(String[] request)
+	{
+		int dayBegin = getTime(request[1], "8", "0");
+		int dayDuration = (23 - 8) * 60;
+		for (Meet meet : meets)
+		{
+			if (((meet.start >= dayBegin && meet.start <= dayBegin + dayDuration)
+			|| (meet.start + meet.duration >= dayBegin && meet.start + meet.duration <= dayBegin + dayDuration))
+			&& isPersonOnMeet(meet, request[2]))
+				printMeet(meet);
+		}
+	}
+
+	public static void printMeet(Meet meet)
+	{
+		int time = meet.start;
+		int minute = time % 60;
+		time -= minute;
+		int hour = time % 1440 / 60;
+		System.out.print(hour + ":" + minute + " " + meet.duration);
+		for (String person : meet.party)
+		{
+			System.out.print(" " + person);
+		}
+		System.out.println();
+	}
+
+	public static boolean isPersonOnMeet(Meet meet, String person)
+	{
+		for (String party : meet.party)
+		{
+			if (party.equals(person))
+				return (true);
+		}
+		return (false);
 	}
 
 	public static int getTime(String day, String hour, String minute)
@@ -64,6 +123,6 @@ public class YandexContest02
 		int numericDay = Integer.parseInt(day);
 		int numericHour = Integer.parseInt(hour);
 		int numericMinute = Integer.parseInt(minute);
-		return (numericDay * 24 * 60 + numericHour * 60 + numericMinute);
+		return ((numericDay - 1) * 24 * 60 + numericHour * 60 + numericMinute);
 	}
 }
